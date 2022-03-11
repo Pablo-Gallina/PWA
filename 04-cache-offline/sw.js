e46ce88406b54a661a6d211b39350dce27e42b67
Pablo Gallina <pablogallinag@gmail.com>
2022-03-11 16:29:25 -0600

    Network with cache #3

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -66,27 +66,57 @@ self.addEventListener('install', event => {
     Intenta primero el cache, y si no encuentras el archivo en el cache, ve al internet
 */
 
-self.addEventListener('fetch', e => {
-    // 1 Primero verificar si el archivo existe en el cache
-    const respuesta = caches.match(e.request)
-        .then(res => {
-            // Si el archivo existe en el cache, reotrnalo
-            if (res) return res;
+// self.addEventListener('fetch', e => {
+//     // 1 Primero verificar si el archivo existe en el cache
+//     const respuesta = caches.match(e.request)
+//         .then(res => {
+//             // Si el archivo existe en el cache, reotrnalo
+//             if (res) return res;
 
-            // No existe el archivo, buscalo en la web
-            return fetch(e.request)
-                .then(newResponse => {
-                    // todas las peticiones que no esten en la app shell, quedaran en el CACHE_DYNAMIC_NAME
-                    caches.open(CACHE_DYNAMIC_NAME)
-                        .then(cache => {
-                            cache.put(e.request, newResponse); // 1 solicitud, 2 lo que contiene la respuesta
-                            limpiarCache( CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT );
-                        })
+//             // No existe el archivo, buscalo en la web
+//             return fetch(e.request)
+//                 .then(newResponse => {
+//                     // todas las peticiones que no esten en la app shell, quedaran en el CACHE_DYNAMIC_NAME
+//                     caches.open(CACHE_DYNAMIC_NAME)
+//                         .then(cache => {
+//                             cache.put(e.request, newResponse); // 1 solicitud, 2 lo que contiene la respuesta
+//                             limpiarCache( CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT );
+//                         })
                     
-                    return newResponse.clone();
-                })
-        });
-    e.respondWith(respuesta);
-})
+//                     return newResponse.clone();
+//                 })
+//         });
+//     e.respondWith(respuesta);
+// })
 
 //!Problemas con el Cache with network fallback, las peticiones (archivos dinamicos) se mesclan con el app Shell, Solucion: separar el caches
+
+
+// * --------------- 3 Network with cache fallback
+// Ir primero al internet y despues al cache
+// Util, por ejemplo para twitter, entras a la web y traes los tweets mas recientes, pero no tienes conexion a internet, entonces te mostrará solo guardados por el cache
+self.addEventListener('fetch', e => {
+    // Primero hacemos la peticion a la web
+    const respuesta = fetch( e.request ).then( res => {
+        // si no se encontro en la web el archivo, revisa en el cache
+        if ( !res ) return caches.match( e.request );
+        
+        // Guardarlo en el cache (sino esta)
+        caches.open( CACHE_DYNAMIC_NAME )
+            .then( cache => {
+                cache.put( e.request, res );
+                limpiarCache( CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT );
+            });
+    
+    
+        return res.clone();
+    
+    }).catch( err =>{ // Si hay algun error (no tenemos conexion a internet), verifica si existe en el cache
+        return caches.match( e.request );
+    });
+
+    // responder con la respuesta
+    e.respondWith(respuesta);
+})
+//!Problemas con el Network with cache fallback, es mucho mas lento que la estrategia 2, porque primero va a la web y luego al cache
+//!Siempre traera lo mas reciente, problema es el consumo de datos, es mucho mayor (no es una principal desventaja pero puede afectar a cierto gurpo de personas)
