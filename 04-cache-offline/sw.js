8c4f6e206bf7d62f399b1d8946aaaca33c064b47
Pablo Gallina <pablogallinag@gmail.com>
2022-03-11 13:23:25 -0600

    Guardando archivos en el cache

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,37 +1,16 @@
-// *Responder un mensaje personalizado cuando no hay conexion a internet
-self.addEventListener('fetch', event => {
-    // ?Crear una respuesta personalizada
-
-
-    // TODO respuesta personalizada html (no recomendable mandarlo asi, pero es funcional)
-    const offlineResp = new Response(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Mi PWA</title>
-
-    </head>
-    <body class="container p-3">
+// * Guardando el app sell (lo que la pagina necesita para que funcione)
+self.addEventListener('install', event => {
+    // ? Almacenar / grabar archivos en el cache
+    const saveCache = caches.open('cache-1')
+        .then(cache => {
+            return cache.addAll([
+                '/index.html',
+                '/css/style.css',
+                '/img/main.jpg',
+                '/js/app.js',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+            ])
+        })
     
-    <h1>Offline Mode</h1>
-    
-    </body>
-    </html>
-    `, {
-        headers: {
-            'Content-Type':'text/html'
-        }
-    });
-
-
-    // const offlineResp = fetch( 'pages/offline.html' ); // respondera mal, porque debe de hacer un fetch, solucion, guardarlo en el cache
-
-    const resp = fetch(event.request)
-                    .catch( () => offlineResp ); // si el fetch falla, retornar una respuesta
-
-    event.respondWith( resp  ); // responder con la respuesta
-
+    event.waitUntil(saveCache); // esperar a que guarde todo, de lo contrario si necesitamos hacer uso de ejemplo, css, podria tirar error porque aun la promesa no ha terminado
 });
