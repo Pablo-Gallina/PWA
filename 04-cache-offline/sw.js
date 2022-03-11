da28aa2ec5f5286eaf61ab844476611f449ff3e9
Pablo Gallina <pablogallinag@gmail.com>
2022-03-11 14:55:20 -0600

    Cache with network fallback #2

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,7 +1,8 @@
+const CACHE_NAME = 'cache-1';
 // * Guardando el app sell (lo que la pagina necesita para que funcione)
 self.addEventListener('install', event => {
     // ? Almacenar / grabar archivos en el cache
-    const saveCache = caches.open('cache-1')
+    const saveCache = caches.open(CACHE_NAME)
         .then(cache => {
             return cache.addAll([
                 '/',
@@ -21,10 +22,37 @@ self.addEventListener('install', event => {
     una vez hagamos la instalacion y tengamos los recursos que la pagina web necesita, entonces jamas regresa a la web (!peticiones)
 */
 
-self.addEventListener('fetch', e => {
-    // lo que este pidiendo en el fetch, responder con
-    e.respondWith(caches.match(e.request)); // si en el cache, encuentra un arachivo que sea igual a la peticion, entonces retorna ese
-})
+// self.addEventListener('fetch', e => {
+//     // lo que este pidiendo en el fetch, responder con
+//     e.respondWith(caches.match(e.request)); // si en el cache, encuentra un arachivo que sea igual a la peticion, entonces retorna ese
+// })
 
 //!Problemas con el cache only, si no se cambia el sw, el cache jamas se actualizara
 
+
+/* // * --------------- 2 Estrategias del cache: Cache with network fallback
+    Intenta primero el cache, y si no encuentras el archivo en el cache, ve al internet
+*/
+
+self.addEventListener('fetch', e => {
+    // 1 Primero verificar si el archivo existe en el cache
+    caches.match(e.request)
+        .then(res => {
+            // Si el archivo existe en el cache, reotrnalo
+            if (res) return res;
+
+            // No existe el archivo, buscalo en la web
+            return fetch(e.request)
+                .then(newResponse => {
+
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(e.request, newResponse); // 1 solicitud, 2 lo que contiene la respuesta
+                        })
+                    
+                    return newResponse.clone();
+                })
+        });
+})
+
+//!Problemas con el Cache with network fallback, las peticiones (archivos dinamicos) se mesclan con el app Shell
