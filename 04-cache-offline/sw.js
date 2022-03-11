a1f0d6327b53cd376c2de1e5f56bbb94f0195b11
Pablo Gallina <pablogallinag@gmail.com>
2022-03-11 14:39:46 -0600

    Cache only #1

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', event => {
     const saveCache = caches.open('cache-1')
         .then(cache => {
             return cache.addAll([
+                '/',
                 '/index.html',
                 '/css/style.css',
                 '/img/main.jpg',
@@ -14,3 +15,16 @@ self.addEventListener('install', event => {
     
     event.waitUntil(saveCache); // esperar a que guarde todo, de lo contrario si necesitamos hacer uso de ejemplo, css, podria tirar error porque aun la promesa no ha terminado
 });
+
+
+/* // * --------------- 1 Estrategias del cache: Cache only 
+    una vez hagamos la instalacion y tengamos los recursos que la pagina web necesita, entonces jamas regresa a la web (!peticiones)
+*/
+
+self.addEventListener('fetch', e => {
+    // lo que este pidiendo en el fetch, responder con
+    e.respondWith(caches.match(e.request)); // si en el cache, encuentra un arachivo que sea igual a la peticion, entonces retorna ese
+})
+
+//!Problemas con el cache only, si no se cambia el sw, el cache jamas se actualizara
+
